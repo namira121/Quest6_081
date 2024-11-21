bdeeb2843d6f423f0646c9ae4ab34a72aaaa4913
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 20:58:55 +0700

    memnambahkan Icon notifikasi di dalam Box dengan warna putih

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
@@ -2,6 +2,7 @@ package com.example.praktikumpam8.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +10,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -71,6 +75,11 @@ fun RencanaStudyView(
                     fontSize = 12.sp,
                     color = Color.White
                 )
+            }
+            Box{
+                Icon(imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White)
             }
         }
     }
