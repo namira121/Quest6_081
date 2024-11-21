8c5da22257d6a822687735a462bcb37100de14ce
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 20:48:26 +0700

    Menambahkan modifier untuk clip agar gambar jadi lingkaran

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -44,7 +46,8 @@ fun RencanaStudyView(
         ) {
             Image(
                 painter = painterResource(id = R.drawable.umy),
-                contentDescription = ""
+                contentDescription = "",
+                modifier = Modifier.clip(shape = CircleShape)
             )
         }
     }
