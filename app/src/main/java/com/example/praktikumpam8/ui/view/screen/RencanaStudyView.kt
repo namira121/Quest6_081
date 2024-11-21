134e6d3e3be07b5edf55cf68bd30cfcc7b11a2ab
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 21:09:51 +0700

    memnambahkan teks di dalam box dengan ukuran 12 dan fontweight light

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
@@ -114,6 +114,9 @@ fun RencanaStudyView(
             )
             Spacer(modifier = Modifier.padding(8.dp))
             Text(text = "Pilih Kelas Belajar", fontWeight = FontWeight.Bold)
+            Text(text = "Silahkan pilih kelas dari mata kuliah yang anda inginkan",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Light)
         }
     }
 }
