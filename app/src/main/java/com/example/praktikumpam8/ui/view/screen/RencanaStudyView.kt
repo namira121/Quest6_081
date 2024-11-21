c36c2c2492d34b454be59e173975423ef77c5e0f
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 21:22:19 +0700

    memnambahkan teks dengan fontweight Bold

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
@@ -141,6 +141,8 @@ fun RencanaStudyView(
             }
             Spacer(modifier = Modifier.padding(8.dp))
             HorizontalDivider()
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(text = "Klausul Persetujuan Mahasiswa", fontWeight = FontWeight.Bold)
         }
     }
 }
