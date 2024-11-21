36f7e37a0187ce77387595826d21e5903158505f
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 22:42:40 +0700

    menambahkan dependensi dan menambahkan paramater pada fungsi MahasiswaApp

--- app/src/main/java/com/example/praktikumpam8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikumpam8/navigation/PengelolaHalaman.kt
@@ -1,8 +1,28 @@
 package com.example.praktikumpam8.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikumpam8.model.Mahasiswa
+import com.example.praktikumpam8.ui.view.viewModel.MahasiswaViewModel
+
 enum class Halaman {
     Splash,
     Mahasiswa,
     MataKuliah,
     Tampil
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel= viewModel(),
+    krsViewModel: MahasiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+
+){
+
 }
