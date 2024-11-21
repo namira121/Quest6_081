259c4fdff80cb4aed57362ca1bf70689acdbe5ab
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 21:14:31 +0700

    memnambahkan Row baru dengan fillmaxwidth dan spaceevenly agar jarak antar elemen sama

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
@@ -2,11 +2,13 @@ package com.example.praktikumpam8.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -120,6 +122,10 @@ fun RencanaStudyView(
                 fontWeight = FontWeight.Light
             )
             Spacer(modifier = Modifier.padding(8.dp))
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ){  }
 
         }
     }
