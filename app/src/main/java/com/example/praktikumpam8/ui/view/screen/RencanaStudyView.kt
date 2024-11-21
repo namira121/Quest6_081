1257cf8837af9b1d9e56c993ffe5484998ba5981
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 20:49:01 +0700

    membuat ukuran gambar menjadi 50 dp

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -47,7 +48,7 @@ fun RencanaStudyView(
             Image(
                 painter = painterResource(id = R.drawable.umy),
                 contentDescription = "",
-                modifier = Modifier.clip(shape = CircleShape)
+                modifier = Modifier.clip(shape = CircleShape).size(50.dp)
             )
         }
     }
