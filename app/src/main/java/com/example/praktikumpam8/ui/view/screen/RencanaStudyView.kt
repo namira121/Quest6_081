be01f3242558899c3b8a9d802f1d9887c9328da2
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 20:55:21 +0700

    membuat ukuran tulisan menjadi 15 dan warna putih

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
@@ -18,10 +18,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.praktikumpam8.R
 import com.example.praktikumpam8.model.Mahasiswa
 import com.example.praktikumpam8.model.RencanaStudy
@@ -60,6 +62,8 @@ fun RencanaStudyView(
             ){
                 Text(text = mahasiswa.nama,
                     fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
                 )
             }
         }
