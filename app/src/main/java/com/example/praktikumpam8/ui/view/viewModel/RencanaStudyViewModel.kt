5320aad776b77b58b1f29fc2608d77bd395596a7
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 21:51:36 +0700

    membuat variabel private krsState, variabel krsstateui dan fungsi setMataKuliah

---
+++ app/src/main/java/com/example/praktikumpam8/ui/view/viewModel/RencanaStudyViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.praktikumpam8.ui.view.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.example.praktikumpam8.model.RencanaStudy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel :ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudy())
+    val krsStateUi:StateFlow<RencanaStudy> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan:String){
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
+    }
+}
