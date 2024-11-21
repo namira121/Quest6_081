56ee39ca62744ed9c8fb4e07bf33eb7c1f39f823
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 20:34:39 +0700

    Menambahkan spacer dengan padding 16

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
@@ -2,7 +2,9 @@ package com.example.praktikumpam8.ui.view.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,6 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.example.praktikumpam8.R
 import com.example.praktikumpam8.model.Mahasiswa
 import com.example.praktikumpam8.model.RencanaStudy
@@ -32,6 +35,6 @@ fun RencanaStudyView(
             .background(color = colorResource(id = R.color.primary)),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Spacer(modifier = Modifier.padding(16.dp))
     }
 }
