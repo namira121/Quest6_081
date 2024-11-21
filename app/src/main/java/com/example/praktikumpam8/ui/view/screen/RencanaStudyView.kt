118667c59c5e5b937a93896323dfaa11590cb97b
Namira Pramadhani <namnam6617@gmail.com>
2024-11-21 21:33:40 +0700

    menambahkan button kembali

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/RencanaStudyView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -158,7 +159,13 @@ fun RencanaStudyView(
             Spacer(modifier = Modifier.padding(8.dp))
             Row(
                 modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
-            ) { }
+            ) {
+                Button(
+                    onClick = {onBackButtonClicked()}
+                ) {
+                    Text(text = "Kembali")
+                }
+            }
         }
     }
 }
