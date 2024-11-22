e9b6f05352e78a1c1656d615e013f77bb20ab83b
Namira Pramadhani <namnam6617@gmail.com>
2024-11-22 08:29:35 +0700

    memnambahkan header untuk detailview

--- app/src/main/java/com/example/praktikumpam8/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/example/praktikumpam8/ui/view/screen/DetailView.kt
@@ -1,23 +1,36 @@
 package com.example.praktikumpam8.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.praktikumpam8.R
 import com.example.praktikumpam8.model.Mahasiswa
 import com.example.praktikumpam8.model.RencanaStudy
 
 @Composable
 fun DetailView(
-    modifier: Modifier,
     uiStateMahasiswa: Mahasiswa,
     uiStateStudy: RencanaStudy,
     onBackButtonClicked: () -> Unit = {}
@@ -29,20 +42,60 @@ fun DetailView(
         Pair("MataKuliah", uiStateStudy.mataKuliah),
         Pair("Kelas", uiStateStudy.kelas),
     )
-
-    Column (
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .background(color = colorResource(id = R.color.primary)),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        listDetail.forEach{ items -> CardSection(
-            judulParam = items.first,
-            isiParam = items.second
-        )
+    ) {
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier.size(45.dp).clip(shape = CircleShape)
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Light
+                )
+            }
         }
-        Button(onClick = {onBackButtonClicked()}) {
-            Text(text = "Kembali")
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(topEnd = 15.dp, topStart = 15.dp)
+                )
+                .fillMaxSize(),
+        ) {
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                listDetail.forEach{ items -> CardSection(
+                    judulParam = items.first,
+                    isiParam = items.second
+                )
+                }
+                Button(onClick = {onBackButtonClicked()}) {
+                    Text(text = "Kembali")
+                }
+            }
         }
     }
 }
